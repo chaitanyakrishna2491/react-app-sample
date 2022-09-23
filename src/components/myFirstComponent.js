bc4102d708b38ae640ba9f17806f7c6c600f20a9
chaitanyakrishna2491 <chaitanyakrishna2491@gmail.com>
2022-09-23 07:22:04 +0530

    adding cmponentdidmount

--- src/components/myFirstComponent.js
+++ src/components/myFirstComponent.js
@@ -1,56 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState, Component } from 'react'
+import axios from 'axios'
 
-function MyFirstComponent(props){
-    const [counterValue, setCounterValue] = useState(props.counter)
+// function MyFirstComponent(props){
+//     const [counterValue, setCounterValue] = useState(props.counter)
 
-    function increment(){
-        setCounterValue(counterValue+1)
-    }
-
-    function decrement(){
-        setCounterValue(counterValue-1)
-    }
-
-    return (
-        <div>
-            Hello {props.uname}!!!
-            <br/>
-            <input type="button" onClick={increment} value="Increment"></input><br/>
-            <input type="button" onClick={decrement} value="Decrement"></input><br/>
-            From myfirstcomp the coounter vale from props {counterValue}
-        </div>
-    )
-}
-
-// class MyFirstComponent extends Component{
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             counterValue : props.counter,
-//         }
-//         this.increment = this.increment.bind(this)
-//         this.decrement = this.decrement.bind(this)
-//     }
-    
-//     decrement(){
-//         this.setState({counterValue: --this.state.counterValue})
+//     function increment(){
+//         setCounterValue(counterValue+1)
 //     }
 
-//     increment() {
-//         this.setState({counterValue: ++this.state.counterValue})
+//     function decrement(){
+//         setCounterValue(counterValue-1)
 //     }
 
-//     render(){
-//         return (
-//             <div>
-//                 Hello {this.props.uname}!!!
-//                 <br/>
-//                 <input type="button" onClick={this.increment} value="Increment"></input><br/>
-//                 <input type="button" onClick={this.decrement} value="Decrement"></input><br/>
-//                 From myfirstcomp the coounter vale from props {this.state.counterValue}
-//             </div>
-//         )
-//     }
+//     return (
+//         <div>
+//             Hello {props.uname}!!!
+//             <br/>
+//             <input type="button" onClick={increment} value="Increment"></input><br/>
+//             <input type="button" onClick={decrement} value="Decrement"></input><br/>
+//             From myfirstcomp the coounter vale from props {counterValue}
+//         </div>
+//     )
 // }
+
+class MyFirstComponent extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            counterValue : props.counter,
+            sampleObj: {
+                first_name:"Loading..."
+            }
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+    }
+    
+    decrement(){
+        this.setState({counterValue: --this.state.counterValue})
+    }
+
+    increment() {
+        this.setState({counterValue: ++this.state.counterValue})
+    }
+
+    componentDidMount(props){
+        setTimeout(() => {
+            axios.get('https://reqres.in/api/users/1').then((response) => {
+                this.setState({sampleObj: response.data.data})
+            })
+        }, 5000)
+    }
+
+    render(){
+        return (
+            <div>
+                Hello {this.state.sampleObj.first_name}!!!
+                <br/>
+                <input type="button" onClick={this.increment} value="Increment"></input><br/>
+                <input type="button" onClick={this.decrement} value="Decrement"></input><br/>
+                From myfirstcomp the coounter vale from props {this.state.counterValue}
+            </div>
+        )
+    }
+}
 
 export default MyFirstComponent;
